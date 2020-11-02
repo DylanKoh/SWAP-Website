2fff2bb4d0c0bdee7d2643c2ed774c2a5135a320
User <User@Nat-PC>
2020-11-02 18:10:36 +0800

    added footer to index page -nat

--- index.php
+++ index.php
@@ -1,7 +1,89 @@
 <?php
-echo "Hello World!";
-echo "I'm Dylan";
-echo "nat will do this page today";
+echo "yireng is a simp"
 ?>
 <html>
+<head>
+</head>
+
+<style>
+
+.col-md-4 a {
+  font-size: 2.5em;
+  padding: 1em;
+}
+
+.fa-facebook {
+  color: #3b5998
+}
+
+.fa-twitter {
+  color: #00aced;
+}
+
+.fa-instagram {
+  color: #517fa4;
+}
+
+.fa-facebook:hover,
+.fa-twitter:hover,
+.fa-instagram:hover {
+  color: #d5d5d5;
+  text-decoration: none;
+}
+
+footer {
+  background-color: #3f3f3f;
+  color: #d5d5d5;
+}
+
+hr.light {
+  border-top: 1px solid #d5d5d5;
+  width: 75%;
+}
+
+</style>
+
+<body>
+
+<footer>
+  <div class="container-fluid padding">
+    <div class="row text-center">
+      <div class="col-md-4">
+        <img src="images/websitelogo.png" alt="Website Logo" style="width: auto; height: 201px;">
+        <hr class="light">
+        <p>Temasek Polytechnic</p>
+        <p>School of IIT</p>
+        <p>21 Tampines Avenue 1, Singapore 529757</p>
+        <p>Tampines, Singapore</p>
+      </div>
+      <div class="col-md-4">
+        <br><br><br><br><br><br>
+        <hr class="light">
+        <h5>Contact Us</h5>
+        <hr class="light">
+        <p>+65 8123 4567</p>
+        <p>1907446g@student.tp.edu.sg</p>
+        <a href="#" class="fab fa-facebook"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+      </div>
+      <div class="col-md-4">
+          <br><br><br><br><br><br>
+          <hr class="light">
+          <h5>About</h5>
+          <hr class="light">
+          <p>Privacy Policy</p>
+          <p>Terms & Conditions</p>
+      </div>
+      <div class="col-12">
+        <hr class="light">
+        <h5>&copy; Website Name</h5>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+
+</body>
 </html>
