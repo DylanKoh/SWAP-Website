d46659feb0257d2bd9d563311cbad3a3468cdf2b
Dylan Koh <1907446g@student.tp.edu.sg>
2020-11-02 17:46:57 +0800

    Added simple code login for Login page for Create button

--- login.php
+++ login.php
@@ -1,10 +1,21 @@
 <html>
 <head><title>Login</title></head>
 <body>
-<?php
+<h1 align="center">Login to your Account</h1>
+<form action="" method="post">
+<table>
+<tr><td>Username: </td><td><input inputmode="text" placeholder="Username" name="username"></td></tr>
+<tr><td>Password: </td><td><input inputmode="text" type="password" placeholder="Password" name="password"></td></tr>
+</table>
+<input type="submit" value="Login" name="btnLogin">
+<input type="submit" value="Create New Account" name="btnCreateAccount">
+</form>
 
+<?php
+if (isset($_POST['btnCreateAccount'])){
+    header("Location: createAccount.php");
+}
 ?>
 </body>
-
 </html>
 
