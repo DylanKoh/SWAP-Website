956517c80e5cc17f09460a9688a7b9d004412022
Dylan Koh <1907446g@student.tp.edu.sg>
2020-11-08 21:21:57 +0800

    Added comment of Psuedo Code in both login pages

--- providerLogin.php
+++ providerLogin.php
@@ -110,6 +110,9 @@ if (isset($_POST["btnLogin"])){
     if (isset($_POST['username']) && isset($_POST['password'])){
         if (strlen($_POST['username']) > 0 && strlen($_POST['password']) > 0){
             echo "Hello Check";
+            //Check if account exist
+            //If exist, Log in or Validate 2FA if applicable
+            //Else, error message
         }
         else{
             alertMessage("Please ensure fills are not empty!");
