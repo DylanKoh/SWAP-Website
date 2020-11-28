6d578b9a39bbbd14a464e88ec9650fc1b152b2e4
chuay <chuay@DESKTOP-U1C9FRC>
2020-11-28 18:43:03 +0800

    Editing the post and viewing function for storepage

--- storePage.php
+++ storePage.php
@@ -1,6 +1,7 @@
 <html>
     <head>
     	<script src="https://kit.fontawesome.com/9d4359df6d.js"></script>
+    	<link rel="stylesheet" type="text/css" href="storepage.css">
     	<title>Pentesters for Hire</title>
     </head>
 
@@ -24,10 +25,12 @@
 		<!-- Store body codes -->
 		<div class="store-body">
 		
+		<div class="store-header1">
 		<h1>Top Reviewed Sellers</h1>
+		<button class='post-but' id='mod-button'>Post A Service</button>
+		</div>
 		
 		<?php 
-		$rows= 0;
 		
 		//Retrieving Service data from database:
 		$stmt= $con->prepare("SELECT services.servicesId, services.serviceName, services.serviceDesc, services.providersFkid, services.price, providers.username FROM services 
@@ -42,185 +45,164 @@
     		echo"<div class='container'><div class='box-view'><div class='sell-info'>";      
     		echo"<p id='title' style='font-size:22px;'><b>". $serviceName . "</b></p>";
     		echo"<p style='font-size:14px;'> Provider: ".$username."</p>";
-    		echo "<p id='sell-price'>Price Range: $100+</p>";
-    		echo"<p id='rating'>5 <i class='fas fa-star fa-sm'></i> <a>(121)</a></p>";
-    		echo"</div> </div> </div>";
+    		echo "<p id='sell-price'>Price: $". $price. "</p>";
+    		echo"<p id='rating'>5 <i class='fas fa-star fa-sm'></i> <a>(No. of Reviews)</a></p>";
+    		echo"</div> </div> </div> </div>";
     		
 		}
 	
 		?>
 		
-		<!-- <div class='sell-column'>
-    		<div class='container'>
-        		<div class='box-view'>
-        			<div class='sell-info'>
-        				<h3>Caleb</h3>
-        				<p id="sell-price">Price Range: $100+</p>
-        				<p>I enjoy pentesting as a hobby</p>
-        				<p id="rating">5 <i class="fas fa-star fa-sm"></i> <a>(121)</a></p>
-        			</div>
-        		</div>
-    		</div>
-    		
-    		<div class="container">
-        		<div class="box-view">
-        			<div class="sell-info">
-        				<h3>Javier</h3>
-        				<p id="sell-price">Price Range: $60+</p>
-        				<p>Have past job experiences</p>
-        				<p id="rating">4.7 <i class="fas fa-star fa-sm"></i> <a>(152)</a></p>
-        			</div>
-        		</div>
-    		</div>
-    		
-    		<div class="container">
-        		<div class="box-view">
-        			<div class="sell-info">
-        				<h3>Jack</h3>
-        				<p id="sell-price">Price Range: $160+</p>
-        				<p>Professional Pen-tester</p>
-        				<p id="rating">4.4 <i class="fas fa-star fa-sm"></i> <a>(32)</a></p>
-        			</div>
-        		</div>
-    		</div>
-    		
-    		<div class="container">
-        		<div class="box-view">
-        			<div class="sell-info">
-        				<h3>Kevin</h3>
-        				<p id="sell-price">Price Range: $45+</p>
-        				<p>I do pentesting as a part time job</p>
-        				<p id="rating">4.2 <i class="fas fa-star fa-sm"></i> <a>(37)</a></p>
-        			</div>
-        		</div>
-    		</div>
-		</div> -->
+		<!-- Modal for Posting data -->
 		
-		<!-- <h1>Lowest Price</h1>
-		<div class="box-view">
-			<div class="sell-info">
-				<h3>Sean </h3>
-				<p id="sell-price">Price Range: $10+</p>
-				<p>New to renting my services</p>
-				<p id="rating">4.9 <i class="fas fa-star fa-sm"></i> <a>(5)</a></p>
-			</div>
+		<div id="postModal" class="modal">
+			
+    		<div class="modal-content">
+        			<div class="modal-header">
+                      <span class="close">&times;</span>
+                      <h2>Post A Service</h2>
+                    </div>
+                    
+                    <form>
+                        <div class="modal-body">
+                        <a>
+                          <label for='sName'><b>Service Name:</b></label> <br>
+                          <input type='text' placeholder='Enter service name' name='serName' required> <br>
+                        </a>
+                        
+                        <a> 
+                          <label for='sDesc'><b>Service Description:</b></label> <br>
+                          <input type='text' placeholder='Enter your service description' name='serDesc' required> <br>
+                        </a>  
+                          
+                        <a>
+                          <label for='sPrice'><b>Price of your service: </b></label> <br>
+                          <input type='text' placeholder='Enter price of service' name='serPrice' required>
+                       	</a>   
+                          
+                          
+                        </div>
+                    </form>
+                    <div class="modal-footer">
+                      <h3></h3>
+        		</div>
+    		</div>
 		</div>
-		-->
+	
 		
 	</div>
     </body>
+    
     <style>
-    body {
-    padding: 0;
-    margin: 0;
-    width: 100%;
-    }
-    h1 {
-    margin-top: 7%;
-    margin-bottom: 3%;
-    }
-    h2, h3, h4, p{
-    margin:0;
-    padding:0;
-    }
-    /*Css for headers */
-    .webhead {
-    height: 80px;
-    background: #003366;
-    font-size: 24px;
-    color: white;
-    }
-    .webhead #left {
-    text-align: center;
-    padding: 25px;
-    width: 15%;
-    float:left;
-    }
-    .webhead #nav-search{
-    margin: 22px 0px;
-    margin-left: 10%;
-    padding: 8px;
-    padding-right: 0px;
-    font-size: 16px;
-    border-radius: 5px;
-    outline-width: 0;
-    }
-    .webhead #nav-sea-but{
-    padding: 8px;
-    font-size: 16px;
-    border-radius: 5px;
-    outline-width: 0;
-    }
-    .webhead-right {
-    float: right;
-    margin: 15px;
-    padding: 10px;
-    }
-    .webhead-right a {
-    margin: 12px;
-    padding: 8px;
-    text-decoration:none;
-    color: white;
-    }
-    .webhead-right .nav-but {
-    border: 1px solid white;
-    border-radius: 3px;
-    }
-    .webhead-right a:hover{
-    color: #DCDCDC;
-    }
-    /*End of Headers */
     
-    /*Start of body */
-    .store-body {
-    width: 80%;
-    margin: auto;
-    height: 1200px;
-    }
-    .sell-column{
-    display:flex;
-    
-    }
-    .sell-column .container{
-    padding-right:20px;
+	.modal {
+      display: none; 
+      position: fixed; 
+      z-index: 1; 
+      left: 0;
+      top: 0;
+      width: 100%; 
+      height: 100%; 
+      overflow: auto; /* Enable scroll if needed */
+      background-color: rgb(0,0,0);
+      background-color: rgba(0,0,0,0.4);
     }
     
-    .box-view {
-    position: relative;
-    width: 260px;
-    height: 320px;
-    background-color:#fdfdf8;
-    overflow: hidden;
-    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.4);
-    border: 1px solid black;
-    }
-    .box-view .sell-info {
-    position: absolute;
-    width: 100%;
-    height: 40%;
-    background-color:lightgray;
-    bottom: 0;
-    }
-    .box-view .sell-info h3 {
-    padding: 1% 6%;
-    }
-    .box-view .sell-info p {
-    padding: 0 6%;
-    }
-    .box-view #rating{
-    font-size:20px;
-    }
-    .box-view i {
-    color:#FFD700;
-    }
-    .box-view a{
-    font-size:16px;
-    }
-    .box-view .sell-info #title {
-    font-size:18px;
-    padding-top: 5px;
+    /* Modal Content/Box */
+    .modal-content {
+      background-color: #fefefe;
+      margin: 10% auto;
+      border: 1px solid #888;
+      height: 500px;
+      width: 60%;
     }
     
+    .modal-header {
+      padding: 2px 24px;
+      background-color: #003366;
+      color: white;
+      height: 60px;
+    }
     
-    </style>
+    .modal-header h2 {
+    margin:18px 0px;
+    }
+    
+    .modal-header span {
+    margin: 8px 2px;
+    font-size: 40px;
+    }
+    
+    .modal-body {
+    padding: 20px 24px;
+    }
+    
+    /* Full-width input fields */
+    .modal-body input[type=text] {
+      width: 60%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      box-sizing: border-box;
+    }
+    
+    .modal-body label {
+    font-size: 20px;
+    }
+    
+    .modal-body a {
+    padding-bottom: 20px;
+    }
+    
+    .modal-footer {
+      padding: 2px 16px;
+      background-color: #5cb85c;
+      color: white;
+    }
+    
+    /* The Close Button */
+    .close {
+      color: #aaa;
+      float: right;
+      font-size: 28px;
+      font-weight: bold;
+    }
+    
+    .close:hover,
+    .close:focus {
+      color: black;
+      text-decoration: none;
+      cursor: pointer;
+    }
+	</style>
+	
+	<script type="text/javascript">
+	
+		//Obtain the modal
+		var modal = document.getElementById("postModal");
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("mod-button");
+        
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+          modal.style.display = "block";
+        }
+        
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+        
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+    }
+    </script>
 </html>
