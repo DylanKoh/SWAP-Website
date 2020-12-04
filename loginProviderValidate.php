f51adce969bd38bac701e5441f0a403eea685a5e
Dylan Koh <1907446g@student.tp.edu.sg>
2020-12-04 15:07:37 +0800

    Added concept test on 2FA Google

--- loginProviderValidate.php
+++ loginProviderValidate.php
@@ -1,2 +1,5 @@
 <?php
+session_set_cookie_params(0, '/', 'localhost', TRUE, TRUE);
+session_start();
+require_once 'PHPGangsta/GoogleAuthenticator.php';
 ?>
--- loginUserValidate.php
+++ loginUserValidate.php
@@ -1,2 +1,7 @@
 <?php
+session_set_cookie_params(0, '/', 'localhost', TRUE, TRUE);
+session_start();
+require_once 'PHPGangsta/GoogleAuthenticator.php';
+$ga=new PHPGangsta_GoogleAuthenticator();
+echo $ga->createSecret();
 ?>
