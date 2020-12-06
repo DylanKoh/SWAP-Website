5b4619f2ed3e564a2130de61967b5b193c1dcc80
nat <User@Nat-PC>
2020-12-06 17:48:41 +0800

    checkoutDelete.php

--- checkoutPost.php
+++ checkoutPost.php
@@ -86,7 +86,13 @@ if($noAdd) {
     } else {
         echo "Unable to update!";
     }
-} 
+} else if($isDelete) {
+    echo "<form action='checkoutDelete.php' method='post'><br>";
+    echo "Are you sure you want to delete your payment information, user " . $userFkid . "? <br><br>";
+    echo "<input type='hidden' name='userFkid' value='".$creditCard."'>";
+    echo "<input type='submit' value='Delete'>";
+    echo "</form>";
+}
 
 
 
