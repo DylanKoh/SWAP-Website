7824859caa904883c6dd2c6d7c2036fc8c32be42
Dylan Koh <1907446g@student.tp.edu.sg>
2020-12-06 14:51:31 +0800

    Added some codes for provider login validate for 2fa

--- loginUserValidate.php
+++ loginUserValidate.php
@@ -1,13 +1,30 @@
+<html>
+<body>
+<p>Please enter your 2FA code from the Google Authenticator Application</p>
+<form action="" method="post">
+Code: <input name="code" type="text"><br>
+<input type="submit" name="btnSubmit" value="Verify Code">
+</form>
+</body>
+</html>
 <?php
+require_once 'PHPGangsta/GoogleAuthenticator.php';
 session_set_cookie_params(0, '/', 'localhost', TRUE, TRUE);
 session_start();
-require_once 'PHPGangsta/GoogleAuthenticator.php';
 require_once 'connection.php';
-if (!isset($_SESSION['userID'])){
-    header('Location:login.php?error=notloggedin');
-    exit();
+if (isset($_POST['btnSubmit']) && isset($_SESSION['providersID'])){
+    if (!empty($_POST['code'])){
+        if (!isset($_SESSION['userID'])){
+            header('Location:login.php?error=notloggedin');
+            exit();
+        }
+        $ga=new PHPGangsta_GoogleAuthenticator();
+        $userGSecret=$_SESSION['googleSecret'];
+        $keyedCode=$_POST['code'];
+        $isVerified=$ga->verifyCode($userGSecret, $keyedCode);
+    }
+    
 }
-$ga=new PHPGangsta_GoogleAuthenticator();
-$userGSecret=$_SESSION['googleSecret'];
+
 
 ?>
