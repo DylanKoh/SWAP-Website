07f35ed0fbe0ee500376aa56af07ceabb1eb8bbb
User <User@Nat-PC>
2020-12-06 18:28:49 +0800

    commitDelete.php

--- checkoutDelete.php
+++ checkoutDelete.php
@@ -1,13 +1,13 @@
 <?php
 include 'connection.php';
 session_start();
-$_SESSION['userID'] ='3';
+$_SESSION['userID'] =3;
 $_SESSION['isUser'] ='yes';
 $isUser = $_SESSION['isUser'];
 $userFkid = $_SESSION['userID'];
 
-$stmt=$conn->prepare("DELETE FROM sales WHERE userFkid=?");
-$stmt->bind_param("i", $userFkid);
+$stmt=$conn->prepare("DELETE FROM sales WHERE usersFkid=?");
+$stmt->bind_param("i", $_SESSION['userID']);
 $res=$stmt->execute();
 if($res) {
     echo "Deleted successfully!";
