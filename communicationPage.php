8e501c84b2fa9ddf21dd520d038c4e52352f0c5f
Darren <Darren@LAPTOP-VMA7KK0D>
2020-12-08 02:58:34 +0800

    communicationPage

--- communicationPage.php
+++ communicationPage.php
@@ -7,13 +7,24 @@ $username = $_GET['username']; //to display username
 <html>
 <head>
 <title>Chat</title>
-<link rel='stylesheet' href='communicationPage.css' />
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="communicationPage.css">
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css">
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
+<header>
+<div class="container">
+<img src="logo.png" alt="logo" class="logo">
+<nav>
+<ul>
+<li><a href="index.php">Home</a></li>
+</ul>
+</nav>
+</div>
+</header>
+
 <div class="chatContainer">
 <div class="chatHeader">
 <h3>Welcome <?php echo ucwords($username)?></h3>
