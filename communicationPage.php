d0a526059e5ce6ba3b21f1fd93514ba3d92cb07b
Darren <Darren@LAPTOP-VMA7KK0D>
2020-12-08 00:29:48 +0800

    communicationPage

--- communicationPage.php
+++ communicationPage.php
@@ -1,8 +1,7 @@
 <?php
 include 'connection.php';
-$users='userId';
-$username='name';
-$username = $_GET['u']; //to display username
+$usersId='usersId';
+$username = $_GET['username']; //to display username
 ?>
 
 <html>
@@ -11,47 +10,48 @@ $username = $_GET['u']; //to display username
 <link rel='stylesheet' href='communicationPage.css' />
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
-<body>
-<div class='chatContainer'>
-<div class='chatHeader'>
-<h3>Welcome <?php echo ucwords($users)?></h3>
-</div>
-<div class='chatMessages'></div>
-<div class='chatBottom'>
-<form method="post" onSubmit='return false;' id='message'>
-<input type='hidden' id='usersId' value='<?php echo $username ;?>'/> 
-<input type='text' name='messageContent' id='messageContent' value='' placeholder='type your chat message' />
-<input type='submit' name='submit' value='Post' />
 
-<?php 
-include 'connection.php';
+<body>
+<div class="chatContainer">
+<div class="chatHeader">
+<h3>Welcome <?php echo ucwords($username)?></h3>
+<div class="chatMessages">
+<div class="chatBottom">
+<form method="post" onSubmit="return false;" id="message">
+<input type="hidden" name="usersId" id="usersId" value="<?php echo $username;?>">
+<input type="text" name="messageContent" id="messageContent" value="" placeholder="Type your chat message">
+<input type="submit" name="submit" value="POST">
+<?php
 //Getting messages
-$query=$conn->prepare("SELECT * FROM message");
+$query=$conn->prepare("SELECT * FROM message WHERE messageContent=?");
 $query->execute();
 
 //Fetch
-while($fetch=$query->fetch(PDO::FETCH_ASSOC)){
+while ($fetch=$query->fetch()) {
     $username = $fetch['username'];
     $messageContent = $fetch['messageContent'];
     
-    echo "<li class='cm'><b>".ucwords($name)."</b> - ".$text."</li>";
-
-//Secure the chat
-    if(isset($_POST['messageContent']) && isset($_POST['username'])){
-        $messageContent = strip_tags(stripslashes(($_POST['messageContent'])));
-        $username = strip_tags(stripslashes(($_POST['username'])));
+    echo "<li class='cm'><b>".ucwords($username)."</b> - ".$messageContent."</li>";
+    
+    //Secure the chat
+    if (isset($_POST['messageContent']) && isset($_POST['username'])) {
+        $messageContent = strip_tags(stripcslashes($_POST['messageContent']));
+        $username = strip_tags(stripcslashes($_POST['username']));
         
-        if(empty($messageContent) OR empty($username))
-        {
-            $insert=$conn->prepare("INSERT INTO message VALUES('',.$messageContent.,'','','','','')");
+        if(empty($messageContent)) {
+            $insert=$conn->prepare("INSERT INTO message VALUES (?,?,?,?,?,?,?)");
             $insert->execute();
             
             echo "<li class='cm'><b>".ucwords($username)."</b> - ".$messageContent."</li>";
         }
-    }  
+    }
+}
 ?>
 </form>
+</div>
+</div>
 </div>
 </div>
 </body>
