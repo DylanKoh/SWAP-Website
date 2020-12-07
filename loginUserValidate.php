dec9007339c1c5dc13da425b3409493ee7894ab2
Dylan Koh <1907446g@student.tp.edu.sg>
2020-12-08 02:07:04 +0800

    Changed session order

--- loginUserValidate.php
+++ loginUserValidate.php
@@ -15,7 +15,7 @@ if (isset($_GET['error']) && $_GET['error'] == 'incorrectcode'){
 </html>
 <?php
 require_once 'PHPGangsta/GoogleAuthenticator.php';
-session_set_cookie_params(0, '/', 'localhost/SWAPWebsite', TRUE, TRUE); //Sets session only visible in HTTPS
+session_set_cookie_params(0, '/', 'localhost', TRUE, TRUE); //Sets session only visible in HTTPS
 session_start(); //Starts session
 require_once 'connection.php';
 if (isset($_POST['btnSubmit']) && isset($_SESSION['userID'])){
