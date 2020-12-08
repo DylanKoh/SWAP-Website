f809cef22e7ee8a9a93da7a650de186da0e33a7f
Dylan Koh <1907446g@student.tp.edu.sg>
2020-12-08 15:28:51 +0800

    Added 2FA check for providers

--- loginProviderValidate.php
+++ loginProviderValidate.php
@@ -1,7 +1,44 @@
+
+<html>
+<body>
+<p>Please enter your 2FA code from the Google Authenticator Application</p>
+
+<form action="" method="post">
+Code: <input name="code" type="text"><br>
 <?php
+if (isset($_GET['error']) && $_GET['error'] == 'incorrectcode'){
+    echo "<p style='color: red;'>Incorrect code!</p>";
+}
+?>
+<input type="submit" name="btnSubmit" value="Verify Code">
+</form>
+</body>
+</html>
+<?php
+require_once 'PHPGangsta/GoogleAuthenticator.php';
 session_set_cookie_params(0, '/', 'localhost', TRUE, TRUE); //Sets session only visible in HTTPS
 session_start(); //Starts session
-require_once 'PHPGangsta/GoogleAuthenticator.php';
-$ga=new PHPGangsta_GoogleAuthenticator();
-echo $ga->createSecret();
+require_once 'connection.php';
+if (isset($_SESSION['providersID'])){
+    if (isset($_POST['btnSubmit'])){
+        if (!empty($_POST['code'])){
+            $ga=new PHPGangsta_GoogleAuthenticator();
+            $userGSecret=$_SESSION['googleSecret'];
+            $keyedCode=$_POST['code'];
+            $isVerified=$ga->verifyCode($userGSecret, $keyedCode);
+            if ($isVerified){
+                header('Location:storePage.php');
+                exit();
+            }
+            else{
+                header('Location:loginProviderValidate.php?error=incorrectcode');
+                exit();
+            }
+        }    
+    }
+}
+else{
+    header('Location:providerLogin.php?error=notloggedin');
+    exit();
+}
 ?>
