872dc37b6b8fea01fad974da2c06d270ee72f7e4
Dylan Koh <1907446g@student.tp.edu.sg>
2020-12-11 01:34:11 +0800

    Added Error check of 2fa code in Create Acc

--- create2FAVad.php
+++ create2FAVad.php
@@ -5,23 +5,35 @@ $ga=new PHPGangsta_GoogleAuthenticator();
 if (isset($_POST['createAccountToken']) && $_POST['createAccountToken'] == $_SESSION['createAccountToken'] && $googleSecret=$_SESSION['googleSecret']){
     $tokenCreateAccountAge=time()-$_SESSION['createAccountTokenTime'];
     if ($tokenCreateAccountAge <= 300 ){ //If token is still below to 5mins old, allow code logic to run
-        $googleSecret=$_SESSION['googleSecret'];
-        $keyedCode=$_POST['verificationCode'];
-        $isVerified=$ga->verifyCode($googleSecret, $keyedCode,0);
-        if ($isVerified){
-            destroySession();
-            echo "Successfully Created an account!<br><br>";
-            echo "<a href='index.php'>Back to Home</a>";
-            exit();
+        if (!empty($_POST['verificationCode']) && preg_match('/^[0-9]{6}$/', $_POST['verificationCode'])){
+            $googleSecret=$_SESSION['googleSecret'];
+            $keyedCode=$_POST['verificationCode'];
+            $isVerified=$ga->verifyCode($googleSecret, $keyedCode,0);
+            if ($isVerified){
+                destroySession();
+                echo "Successfully Created an account!<br><br>";
+                echo "<a href='index.php'>Back to Home</a>";
+                exit();
+            }
+            else{
+                $createAccountToken=$_POST['createAccountToken'];
+                echo "<form action='createAccountDo.php?error=incorrectcode' id='resubmitForm' method='post'>";
+                echo "<input hidden name='createAccountToken' value='$createAccountToken'>";
+                echo "</form>";
+                echo "<script type='text/javascript'>
+      document.getElementById('resubmitForm').submit();
+    </script>";
+                exit();
+            }
         }
         else{
             $createAccountToken=$_POST['createAccountToken'];
-            echo "<form action='createAccountDo.php?error=incorrectcode' id='resubmitForm' method='post'>";
+            echo "<form action='createAccountDo.php?error=invalidCharacters' id='resubmitForm' method='post'>";
             echo "<input hidden name='createAccountToken' value='$createAccountToken'>";
             echo "</form>";
             echo "<script type='text/javascript'>
-  document.getElementById('resubmitForm').submit();
-</script>";
+      document.getElementById('resubmitForm').submit();
+    </script>";
             exit();
         }
     }
--- createAccountDo.php
+++ createAccountDo.php
@@ -163,18 +163,22 @@ if (isset($_POST['createAccountToken']) && $_POST['createAccountToken'] == $_SES
             
         }
         else{
+            $googleSecret=$_SESSION['googleSecret'];
+            $username=$_SESSION['username'];
+            $createAccountToken=$_POST['createAccountToken'];
+            $getQRCodeURL=$ga->getQRCodeGoogleUrl("SWAPWebsite ($username)",$googleSecret);
+            echo "<form action='create2FAVad.php' method='post'>";
+            echo "<img src='$getQRCodeURL' title='Scan on Google 2FA Application' />";
+            echo "<br><input name='verificationCode'>";
+            echo "<br><input type='submit' value='Verify'>";
+            echo "<input hidden name='createAccountToken' value='$createAccountToken'>";
+            echo "</form>";
             if (isset($_GET['error']) && $_GET['error']=='incorrectcode'){
-                $googleSecret=$_SESSION['googleSecret'];
-                $username=$_SESSION['username'];
-                $createAccountToken=$_POST['createAccountToken'];
-                $getQRCodeURL=$ga->getQRCodeGoogleUrl("SWAPWebsite ($username)",$googleSecret);
-                echo "<form action='create2FAVad.php' method='post'>";
-                echo "<img src='$getQRCodeURL' title='Scan on Google 2FA Application' />";
-                echo "<br><input name='verificationCode'>";
-                echo "<br><input type='submit' value='Verify'>";
-                echo "<input hidden name='createAccountToken' value='$createAccountToken'>";
-                echo "</form>";
+                
                 echo "<p style='color: red;'>Incorrect code!</p>";
+            }
+            elseif (isset($_GET['error']) && $_GET['error']=='invalidCharacters'){
+                echo "<p style='color: red;'>Code consist of only 6 numerical characters!</p>";
             }
         }
     }
