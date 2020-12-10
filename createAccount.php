2c4075c8cd4f1a2e8933709012d986ce2c6d380c
Dylan Koh <1907446g@student.tp.edu.sg>
2020-12-10 22:35:01 +0800

    Updated css for CreateAccount

--- createAccount.php
+++ createAccount.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['googleSecret'])){
 ?>
 <html>
 <head>
-      <script src="css/kitfontawesome9d4359df6d.js"/>
+      <script src="css/kitfontawesome9d4359df6d.js"></script>
       <!--bootstrap-->
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
