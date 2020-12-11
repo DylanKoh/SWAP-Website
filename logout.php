b5df43c82f07c5a042e1e5c37e802c0fa54b124d
Dylan Koh <1907446g@student.tp.edu.sg>
2020-12-11 17:52:55 +0800

    fixed code bug for destroying session

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline'");
 header("X-Frame-Options: DENY");
 require_once 'sessionInitialise.php';
-sessionDestroy();
+destroySession();
 ?>
 <head>
   <title>Successful Logout Page</title>
