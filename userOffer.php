705bd45aa5fe7848917fa4a88f1e64bb0ee1bd1a
chuay <chuay@DESKTOP-U1C9FRC>
2020-12-12 02:35:49 +0800

    Diverting the css

--- userOffer.php
+++ userOffer.php
@@ -19,6 +19,7 @@ $servID = '1';
 	<head>
         <!-- linking all relevant css  -->
         <link rel="stylesheet" type="text/css" href="css/header.css">
+        <link rel="stylesheet" type="text/css" href="css/userOffer.css">
     	<title></title>
 	</head>
 	<body>
@@ -89,110 +90,5 @@ $servID = '1';
 		</div>
 	</body>
 	
-<style>
 
-body {
-background:	#F0F8FF;
-height: 800px;
-}
-
-h1, h2, h3, h4, p{
-padding: 0px;
-margin: 0px;
-}
-
-.orderbody {
-width:60%;
-height:80%;
-background-color:white;
-margin: 50px auto;
-}
-
-.orderbody .orderhead h1 {
-text-align: center;
-padding: 10px 0px;
-font-size: 40px;
-border-bottom: 1px solid black;
-}
-.cover-contain{
-display:flex;
-height:100%;
-}
-
-.serv-container {
-display: inline-block;
-width: 40%;
-height: 60%;
-margin-top:2%;
-margin-left:2%;
-}
-
-/* service information box */
-.serv-info{
-border: 1px solid black;
-height:50%;
-}
-
-.serv-info h1{
-margin-left:2%;
-margin-top: 2%;
-}
-
-.serv-info p{
-font-size:18px;
-margin-left:2%;
-}
-
-/* provider box information */
-.prov-info{
-margin-top: 5%;
-border: 1px solid black;
-height:30%;
-}
-
-.prov-info h2{
-margin-left:2%;
-margin-top: 2%;
-}
-
-.prov-info p{
-font-size:18px;
-margin-left:2%;
-}
-
-
-.orderPost{
-margin-top: 2%;
-margin-left:5%;
-width:50%;
-height:400px;
-border:1px solid black;
-}
-
-.orderPost h3{
-margin-left:3%;
-margin-top: 2%;
-}
-
-.orderPost .com-box{
-margin-left:3%;
-margin-top: 1%;
-height:40%;
-width:70%;
-font-size:16px;
-}
-.orderPost .check-field{
-margin-left:3%;
-margin-top: 2%;
-}
-
-.orderPost .order-but{
-margin-left:3%;
-margin-top: 5%;
-}
-
-
-
-
-</style>
 </html>
