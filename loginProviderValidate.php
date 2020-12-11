4dc1f3b4b84c8d5c50d770005fd0f8b59e337644
Dylan Koh <1907446g@student.tp.edu.sg>
2020-12-11 15:43:10 +0800

    comments on loginProviderValidate

--- loginProviderValidate.php
+++ loginProviderValidate.php
@@ -10,7 +10,7 @@ header("X-Frame-Options: DENY");
 Code: <input name="code" type="text"><br>
 <input hidden name='2FAToken' value="<?php echo $_POST["2FAToken"]?>">
 <?php
-if (isset($_GET['error']) && $_GET['error'] == 'incorrectcode'){
+if (isset($_GET['error']) && $_GET['error'] == 'incorrectcode'){ //If GET error message = 'incorrectcode', print red text error message
     echo "<p style='color: red;'>Incorrect code!</p>";
 }
 ?>
@@ -19,10 +19,10 @@ if (isset($_GET['error']) && $_GET['error'] == 'incorrectcode'){
 </body>
 </html>
 <?php
-require_once 'PHPGangsta/GoogleAuthenticator.php';
-require_once 'sessionInitialise.php';
-require_once 'alertMessageFunc.php';
-if (isset($_SESSION['providersID'])){
+require_once 'PHPGangsta/GoogleAuthenticator.php'; //Require Google 2FA code
+require_once 'sessionInitialise.php'; //Require session initialisation
+require_once 'alertMessageFunc.php'; //Require alert function
+if (isset($_SESSION['providersID'])){ //Check if session variable 'providersID' is set. If so, user is validated and logged in and run code
     if (isset($_POST['2FAToken']) && $_POST['2FAToken'] == $_SESSION['2FAToken'])  //Check if token valid
     {
         $token2FAAge=time()-$_SESSION['2FATokenTime'];
@@ -33,7 +33,7 @@ if (isset($_SESSION['providersID'])){
                     $userGSecret=$_SESSION['googleSecret'];
                     $keyedCode=$_POST['code'];
                     $isVerified=$ga->verifyCode($userGSecret, $keyedCode);
-                    if ($isVerified){
+                    if ($isVerified){ //If keyed code is verified and correct, run code
                         unsetVariable('2FAToken');
                         unsetVariable('2FATokenTime');
                         unsetVariable('googleSecret');
@@ -48,31 +48,31 @@ if (isset($_SESSION['providersID'])){
 </script>";
                         exit();
                     }
-                    else{
+                    else{ //If keyed 2FA code is not correct, redirect to self with GET error message
                         header('Location:loginProviderValidate.php?error=incorrectcode');
                         exit();
                     }
                 }
-                else{
+                else{ //If keyed 2FA code is not 6 numerical characters, alert user
                     promptMessage('Allowed code is only 6 numeric characters!');
                 }
             }
             
         }
-        else{
+        else{ //If token has expired, destroy session and force user to re-login
             destroySession();
             header('Location:providerLogin.php?error=sessionExpired');
             exit();
         }
         
     }
-    else{
+    else{ //If token is not valid, destroy session and a re-login is a must
         destroySession();
         header('Location:providerLogin.php?error=invalidToken');
         exit();
     }
 }
-else{
+else{ //If session variable for providersID is not set, user is not logged in and session is destroyed (if any). A re-login is a must
     destroySession();
     header('Location:providerLogin.php?error=notloggedin');
     exit();
