98c708d7d9e2291230d44d28c78cb07250550d1e
Dylan Koh <1907446g@student.tp.edu.sg>
2020-12-11 20:21:38 +0800

    Session continuity

--- StorePost.php
+++ StorePost.php
@@ -3,10 +3,39 @@ header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-i
 header("X-Frame-Options: DENY");
 require_once 'sessionInitialise.php';
 if(!isset($_SESSION['usersID']) && !isset($_SESSION['providersID'])){
-    
+    destroySession();
+    header('Location:login.php?error=notloggedin');
+    exit();
 }
 else{
-    
+    if (isset($_POST['authToken']) && $_POST['authToken'] == $_SESSION['authToken']){
+        $sessionAge=time()-$_SESSION['authTokenTime'];
+        if ($sessionAge > 1200){
+            if (isset($_SESSION['providersID'])){
+                destroySession();
+                header('Location:providerLogin.php?error=sessionExpired');
+                exit();
+            }
+            else{
+                destroySession();
+                header('Location:login.php?error=sessionExpired');
+                exit();
+            }
+        }
+    }
+    else{
+        if (isset($_SESSION['providersID'])){
+            destroySession();
+            header('Location:providerLogin.php?error=invalidToken');
+            exit();
+        }
+        else{
+            destroySession();
+            header('Location:login.php?error=invalidToken');
+            exit();
+        }
+        
+    }
 }
     //Connecting to Mysql Database
     include 'connection.php';
