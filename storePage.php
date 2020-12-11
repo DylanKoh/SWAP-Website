45cb5e71e477adbff105bbe93524cf54838c5d87
Dylan Koh <1907446g@student.tp.edu.sg>
2020-12-11 17:18:13 +0800

    Check Session Time

--- storePage.php
+++ storePage.php
@@ -3,10 +3,37 @@ header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-i
 header("X-Frame-Options: DENY");
 require_once 'sessionInitialise.php';
 if(!isset($_SESSION['usersID']) && !isset($_SESSION['providersID'])){
-    
+    destroySession();
+    header('Location:login.php?error=notloggedin');
+    exit();
 }
 else{
-    
+    if (isset($_POST['authToken']) && $_POST['authToken'] == $_SESSION['authToken']){
+        $sessionAge=time()-$_SESSION['authTokenTime'];
+        if ($sessionAge > 1200){
+            destroySession();
+            if (isset($_SESSION['providersID'])){
+                header('Location:providerLogin.php?error=sessionExpired');
+                exit();
+            }
+            else{
+                header('Location:login.php?error=sessionExpired');
+                exit();
+            }
+        }
+    }
+    else{
+        destroySession();
+        if (isset($_SESSION['providersID'])){
+            header('Location:providerLogin.php?error=invalidToken');
+            exit();
+        }
+        else{
+            header('Location:login.php?error=invalidToken');
+            exit();
+        }
+        
+    }
 }
 ?>
 <html>
