290ef993c0e85e39eea7aaf6e66d246c664a27b7
Dylan Koh <1907446g@student.tp.edu.sg>
2020-12-11 12:57:06 +0800

    Comments on loginUserValidate

--- loginUserValidate.php
+++ loginUserValidate.php
@@ -22,19 +22,19 @@ if (isset($_GET['error']) && $_GET['error'] == 'incorrectcode'){
 require_once 'PHPGangsta/GoogleAuthenticator.php';
 require_once 'sessionInitialise.php';
 require_once 'alertMessageFunc.php';
-if (isset($_SESSION['usersID'])){
+if (isset($_SESSION['usersID'])){ //Checks if session value 'usersID' is set before running page
     if (isset($_POST['2FAToken']) && $_POST['2FAToken'] == $_SESSION['2FAToken'])  //Check if token valid
     {
-        $token2FAAge=time()-$_SESSION['2FATokenTime'];
-        if ($token2FAAge <= 180 ){ //If token is still below to 3mins old, allow code logic to run
+        $token2FAAge=time()-$_SESSION['2FATokenTime']; //Cacluate token age
+        if ($token2FAAge <= 180 ){ //If token age is still below to 3mins old, allow code logic to run
             if (isset($_POST['btnSubmit'])){
-                if (!empty($_POST['code']) && preg_match('/^[0-9]{6}$/', $_POST['code'])){
-                    $ga=new PHPGangsta_GoogleAuthenticator();
+                if (!empty($_POST['code']) && preg_match('/^[0-9]{6}$/', $_POST['code'])){ //Check if code only have  6 numeric characters, if so, run
+                    $ga=new PHPGangsta_GoogleAuthenticator(); //Initialise the Google Authenticator class
                     $userGSecret=$_SESSION['googleSecret'];
                     $keyedCode=$_POST['code'];
-                    $isVerified=$ga->verifyCode($userGSecret, $keyedCode);
-                    if ($isVerified){
-                        unsetVariable('2FAToken');
+                    $isVerified=$ga->verifyCode($userGSecret, $keyedCode); //Create if user's googleSecret and inputted code corresponds with Google's generated code
+                    if ($isVerified){ //If user's code is verified, run code
+                        unsetVariable('2FAToken'); 
                         unsetVariable('2FATokenTime');
                         unsetVariable('googleSecret');
                         $authToken=hash('sha256', uniqid(rand(), TRUE));
@@ -43,37 +43,35 @@ if (isset($_SESSION['usersID'])){
                         echo "<form action='storePage.php' id='submitForm' method='post'>";
                         echo "<input hidden name='authToken' value='$authToken'>";
                         echo "</form>";
-                        echo "<script type='text/javascript'>
-  document.getElementById('submitForm').submit();
-</script>";
+                        echo "<script type='text/javascript'>document.getElementById('submitForm').submit();</script>";
                         exit();
                     }
-                    else{
+                    else{ //If user's code is incorrect, redirect user to same page with GET error message
                         header('Location:loginUserValidate.php?error=incorrectcode');
                         exit();
                     }
                     
                 }
-                else{
+                else{ //If code contains illegal characters, prompt user with message and do not run verification
                     promptMessage('Allowed code is only 6 numeric characters!');
                 }
             }
             
         }
-        else{
+        else{ //If session has expired, user is redirect back to login page with GET message. Session will be destroyed and a re-login is a must
             destroySession();
             header('Location:login.php?error=sessionExpired');
             exit();
         }
         
     }
-    else{
+    else{ //If session is not valid, user is redirect back to login page with GET message. Session will be destroyed and a re-login is a must
         destroySession();
         header('Location:login.php?error=invalidToken');
         exit();
     }
 }
-else{
+else{ //If user access page without a session with usersID value, user is redirect back to login page with GET message. Session will be destroyed and a re-login is a must
     destroySession();
     header('Location:login.php?error=notloggedin');
     exit();
