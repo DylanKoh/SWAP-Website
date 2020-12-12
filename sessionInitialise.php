77ab0be4873008082def109aca04e36091bbd9a2
Dylan Koh <1907446g@student.tp.edu.sg>
2020-12-12 13:46:42 +0800

    Comment on Func files

--- sessionInitialise.php
+++ sessionInitialise.php
@@ -1,4 +1,5 @@
 <?php
+//This code file is written by Dylan Koh for easy and clean session initialisation and management
 session_set_cookie_params(0, '/', 'localhost', TRUE, TRUE); //Sets session only visible in HTTPS
 session_start();
 function initialiseSessionVar($variable, $value) {
--- validateToken.php
+++ validateToken.php
@@ -1,4 +1,5 @@
 <?php
+//This code file is written by Dylan Koh for easy and clean token validation
 require_once 'sessionInitialise.php';
 function verifyToken($tokenName, $ttl) {
     if (isset($_POST["$tokenName"]) && $_POST["$tokenName"] == $_SESSION["$tokenName"]){
