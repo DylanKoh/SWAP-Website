2cbccdee4312360f787f7f1a262b0b0afd597063
Darren <Darren@LAPTOP-VMA7KK0D>
2020-12-13 21:45:59 +0800

    comm

--- communicationPage.php
+++ communicationPage.php
@@ -4,6 +4,8 @@ header("X-Frame-Options: DENY");
 
 include 'connection.php';
 
+$username = $_SESSION['username'];
+
 require_once 'sessionInitialise.php';
 require_once 'validateToken.php';
 if(!isset($_SESSION['usersID']) && !isset($_SESSION['providersID'])){ //If user does not have any ID in their session
@@ -69,7 +71,7 @@ else{ //If an ID of sorts is assigned in the session variables
 <div class="chatMessages">
 <div class="chatBottom">
 <form action="communicationPage.php" method="post">
-<input type="hidden" name="usersId" id="usersId" value="<?php echo $_SESSION['username'];?>">
+<input type="hidden" name="usersId" id="usersId" value="<?php echo htmlspecialchars($username);?>">
 <input type="text" name="messageContent" value="" placeholder="Type your chat message">
 <input type="submit" name="submit" value="submit">
 
